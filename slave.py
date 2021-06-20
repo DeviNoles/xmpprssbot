116952012bcb211edb2f192f7c470af3c3e5ab49
DevinM4 <devin8821@gmail.com>
2021-06-20 01:01:45 -0400

    Get RSS urls from a .ini file

--- slave.py
+++ slave.py
@@ -1,9 +1,10 @@
 import traceback
 import urllib3
 import xmltodict
-
-def getChinaRSS():
-    url = ""	
+import configparser
+import io
+def getRSS(urlIn):
+    url = urlIn
     http = urllib3.PoolManager()
 
     response = http.request('GET', url)
@@ -14,10 +15,22 @@ def getChinaRSS():
         #print(data['feed']['entry'][0]['title']['#text'])
     except:
         print("Failed to parse xml from response (%s)" % traceback.format_exc())
-    
+
    # print(titles['feed']['entry'][0]['title']['#text'])
     for x in titles['feed']['entry']:
         print(x['title']['#text'])
     return data
 
-getChinaRSS()
+def parseConfig():
+    Config = configparser.ConfigParser()
+    Config.read("slaveconfig.ini")
+    #print(Config.sections())
+    for rs in Config.sections():
+        options = Config.options(rs)
+        for option in options:
+            try:
+             #   print(Config.get(rs, option))
+                getRSS(Config.get(rs, option))
+            except:
+                print("WTF")
+parseConfig()
